f54cedec30199349a6d9a347a45828eaaeb313c4
Roderic Campbell <roderic@gmail.com>
2020-07-05 23:04:25 -0700

    Set MacOS 10_15 as minimum

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "VaporEcobee",
+    platforms: [
+       .macOS(.v10_15)
+    ],
     products: [
         .library(name: "VaporEcobee", targets: ["VaporEcobee"])
     ],
