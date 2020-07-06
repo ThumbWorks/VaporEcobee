d82361b22dc888aaec3c9a05e8f72c81b2a00583
Roderic Campbell <roderic@gmail.com>
2020-07-05 22:55:35 -0700

    Update the package generating format

--- Package.swift
+++ Package.swift
@@ -4,18 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "VaporEcobee",
-    platforms: [
-       .macOS(.v10_15)
-    ],
     products: [
         .library(name: "VaporEcobee", targets: ["VaporEcobee"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Vapor/Vapor.git", .exact("4.14.0")),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.14.0"),
     ],
     targets: [
         .target(name: "VaporEcobee", dependencies: [
-          "Vapor",
+          .product(name: "Vapor", package: "vapor")
         ], path: "Sources")
     ]
 )
